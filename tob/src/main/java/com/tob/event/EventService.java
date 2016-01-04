ecb3635b9dab2fb3df10e97d6404a92fefd11be5
june2ne1@naver.com <june2ne1@naver.com>
2016-01-04 18:12:12 +0900

    프로젝트 첫번째날 event

---
+++ tob/src/main/java/com/tob/event/EventService.java
@@ -0,0 +1,11 @@
+package com.tob.event;
+
+import java.util.List;
+
+public interface EventService {
+	public int join(EventVO o);
+	public int change(EventVO o);
+	public int remove(String evtName);
+	public int count();
+	public List<EventVO> getList();
+}
