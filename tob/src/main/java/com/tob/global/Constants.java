75674d1b0953b7f432f5d1991920cefd0ef24083
pheonix0717@naver.com <pheonix0717@naver.com>
2016-01-05 16:09:01 +0900

    constants 수정 pm 4:09

--- tob/src/main/java/com/tob/global/Constants.java
+++ tob/src/main/java/com/tob/global/Constants.java
@@ -12,8 +12,8 @@ public class Constants {
 	public static final String ORACLE_PASSWORD = "movie";
 	public static final String MYSQL_DRIVER = "com.mysql.jdbc.Driver";
 	public static final String MYSQL_URL = "jdbc:mysql://localhost:3306/pheonix0717";
-	public static final String MYSQL_ID = "TOB";
-	public static final String MYSQL_PASSWORD = "HANBIT0717";
+	public static final String MYSQL_ID = "system";
+	public static final String MYSQL_PASSWORD = "mysql";
 	public static final String HSQL_DRIVER = "org.hsqldb.jdbcDriver";
 	public static final String HSQL_URL = "jdbc:hsqldb:C:/jse/MyDB;readonly=true";
 	public static final String HSQL_ID = "sa";
