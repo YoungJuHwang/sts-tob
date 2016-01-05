ffce905f4c8ee6431f4a6cb6aa5c73d0c6bf4734
pheonix0717@naver.com <pheonix0717@naver.com>
2016-01-05 12:44:05 +0900

    멤버 모델 완성

--- tob/src/main/java/com/tob/purchase/PurchaseServiceImpl.java
+++ tob/src/main/java/com/tob/purchase/PurchaseServiceImpl.java
@@ -8,41 +8,38 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tob.mapper.AccountMapper;
+import com.tob.mapper.PerchaseMapper;
+
   
 @Service
 public class PurchaseServiceImpl implements PurchaseService{
 	private static final Logger logger = LoggerFactory.getLogger(PurchaseServiceImpl.class);
-	@Autowired private SqlSession sqlsession;
+	@Autowired private SqlSession sqlSession;
 	
 	
 	@Override
 	public int add(PurchaseVO o) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 	@Override
 	public PurchaseVO searchBypurNum(String purNum) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
 	public List<PurchaseVO> searchByAccNum(String accountNum) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
 	public List<PurchaseVO> getList() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 	@Override
 	public int change(PurchaseVO o) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 	@Override
 	public int remove(String purNum) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 }
